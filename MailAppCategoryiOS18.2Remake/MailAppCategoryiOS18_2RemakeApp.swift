675e6bd389843cf36e8622bc8ef83deff96058c5
Mansur Ahmed <demharusnam@icloud.com>
2024-12-12 16:34:32 -0800

    Initial Commit

---
+++ MailAppCategoryiOS18.2Remake/MailAppCategoryiOS18_2RemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailAppCategoryiOS18_2RemakeApp.swift
+//  MailAppCategoryiOS18.2Remake
+//
+//  Created by Mansur Ahmed on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MailAppCategoryiOS18_2RemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
